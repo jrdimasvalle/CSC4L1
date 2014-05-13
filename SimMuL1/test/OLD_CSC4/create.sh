115c5a8d66282b3d12a2712d38b33dea8071c67a
Jose Dimas <jrdv009@neo.tamu.edu>
2014-05-13 08:42:06 -0500

    This is the L1 config I use. Find the working versino inside PSS and PAA (Not really creative names thou)

---
+++ SimMuL1/test/OLD_CSC4/create.sh
@@ -0,0 +1,4 @@
+crab -create -cfg runL1_SLHC.cfg
+crab -create -cfg runL1_Step1.cfg
+crab -create -cfg runL1_Step2.cfg
+
---
+++ SimMuL1/test/OLD_CSC4/submit.sh
@@ -0,0 +1,3 @@
+crab -submit -c crab_runL1_SLHC
+crab -submit -c crab_runL1_Step1
+crab -submit -c crab_runL1_Step2
